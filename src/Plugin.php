692855ccba49591b29be94147d1fbe0a1b18d171
Joe Huss <detain@interserver.net>
2022-07-28 17:05:45 -0400

    formatting update

--- src/Plugin.php
+++ src/Plugin.php
@@ -12,140 +12,140 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  */
 class Plugin
 {
-	public static $name = 'Parallels Licensing';
-	public static $description = 'Allows selling of Parallels Server and VPS License Types.  More info at https://parallels.com';
-	public static $help = 'Professional control panel that gives web-designers, web-masters and website owners tools to manage their servers, sites and applications. The only hosting solution that will grow with your business from a single site and servers to a multi-server cloud solution and millions of users. The professionals choice for growing businesses.';
-	public static $module = 'licenses';
-	public static $type = 'service';
+    public static $name = 'Parallels Licensing';
+    public static $description = 'Allows selling of Parallels Server and VPS License Types.  More info at https://parallels.com';
+    public static $help = 'Professional control panel that gives web-designers, web-masters and website owners tools to manage their servers, sites and applications. The only hosting solution that will grow with your business from a single site and servers to a multi-server cloud solution and millions of users. The professionals choice for growing businesses.';
+    public static $module = 'licenses';
+    public static $type = 'service';
 
-	/**
-	 * Plugin constructor.
-	 */
-	public function __construct()
-	{
-	}
+    /**
+     * Plugin constructor.
+     */
+    public function __construct()
+    {
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function getHooks()
-	{
-		return [
-			self::$module.'.settings' => [__CLASS__, 'getSettings'],
-			self::$module.'.activate' => [__CLASS__, 'getActivate'],
-			self::$module.'.reactivate' => [__CLASS__, 'getActivate'],
-			self::$module.'.deactivate' => [__CLASS__, 'getDeactivate'],
-			self::$module.'.deactivate_ip' => [__CLASS__, 'getDeactivate'],
-			self::$module.'.deactivate_key' => [__CLASS__, 'getDeactivateKey'],
-			'function.requirements' => [__CLASS__, 'getRequirements']
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function getHooks()
+    {
+        return [
+            self::$module.'.settings' => [__CLASS__, 'getSettings'],
+            self::$module.'.activate' => [__CLASS__, 'getActivate'],
+            self::$module.'.reactivate' => [__CLASS__, 'getActivate'],
+            self::$module.'.deactivate' => [__CLASS__, 'getDeactivate'],
+            self::$module.'.deactivate_ip' => [__CLASS__, 'getDeactivate'],
+            self::$module.'.deactivate_key' => [__CLASS__, 'getDeactivateKey'],
+            'function.requirements' => [__CLASS__, 'getRequirements']
+        ];
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getActivate(GenericEvent $event)
-	{
-		$serviceClass = $event->getSubject();
-		if ($event['category'] == get_service_define('PARALLELS')) {
-			myadmin_log(self::$module, 'info', 'Parallels Activation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			function_requirements('activate_parallels');
-			if (trim($event['field2']) != '') {
-				$response = activate_parallels($serviceClass->getIp(), $event['field1'], $event['field2']);
-			} else {
-				$response = activate_parallels($serviceClass->getIp(), $event['field1']);
-			}
-			myadmin_log(self::$module, 'info', 'Response: '.json_encode($response), __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			$serviceExtra = $response['mainKeyNumber'].','.$response['productKey'];
-			$serviceClass
-				->setKey($response['mainKeyNumber'])
-				->setExtra($serviceExtra)
-				->save();
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getActivate(GenericEvent $event)
+    {
+        $serviceClass = $event->getSubject();
+        if ($event['category'] == get_service_define('PARALLELS')) {
+            myadmin_log(self::$module, 'info', 'Parallels Activation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            function_requirements('activate_parallels');
+            if (trim($event['field2']) != '') {
+                $response = activate_parallels($serviceClass->getIp(), $event['field1'], $event['field2']);
+            } else {
+                $response = activate_parallels($serviceClass->getIp(), $event['field1']);
+            }
+            myadmin_log(self::$module, 'info', 'Response: '.json_encode($response), __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            $serviceExtra = $response['mainKeyNumber'].','.$response['productKey'];
+            $serviceClass
+                ->setKey($response['mainKeyNumber'])
+                ->setExtra($serviceExtra)
+                ->save();
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getDeactivate(GenericEvent $event)
-	{
-		$serviceClass = $event->getSubject();
-		if ($event['category'] == get_service_define('PARALLELS')) {
-			myadmin_log(self::$module, 'info', 'Parallels Deactivation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			function_requirements('deactivate_parallels');
-			$event['success'] = deactivate_parallels($serviceClass->getIp());
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getDeactivate(GenericEvent $event)
+    {
+        $serviceClass = $event->getSubject();
+        if ($event['category'] == get_service_define('PARALLELS')) {
+            myadmin_log(self::$module, 'info', 'Parallels Deactivation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            function_requirements('deactivate_parallels');
+            $event['success'] = deactivate_parallels($serviceClass->getIp());
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getDeactivateKey(GenericEvent $event)
-	{
-		$serviceClass = $event->getSubject();
-		if ($event['category'] == get_service_define('PARALLELS')) {
-			myadmin_log(self::$module, 'info', 'Parallels Deactivation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			function_requirements('deactivate_parallels');
-			$event['success'] = deactivate_parallels_by_key($serviceClass->getKey());
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getDeactivateKey(GenericEvent $event)
+    {
+        $serviceClass = $event->getSubject();
+        if ($event['category'] == get_service_define('PARALLELS')) {
+            myadmin_log(self::$module, 'info', 'Parallels Deactivation', __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            function_requirements('deactivate_parallels');
+            $event['success'] = deactivate_parallels_by_key($serviceClass->getKey());
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getChangeIp(GenericEvent $event)
-	{
-		if ($event['category'] == get_service_define('PARALLELS')) {
-			$serviceClass = $event->getSubject();
-			$settings = get_module_settings(self::$module);
-			$parallels = new \Parallels(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
-			myadmin_log(self::$module, 'info', 'IP Change - (OLD:'.$serviceClass->getIp().") (NEW:{$event['newip']})", __LINE__, __FILE__, self::$module, $serviceClass->getId());
-			$result = $parallels->editIp($serviceClass->getIp(), $event['newip']);
-			if (isset($result['faultcode'])) {
-				myadmin_log(self::$module, 'error', 'Parallels editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__, self::$module, $serviceClass->getId());
-				$event['status'] = 'error';
-				$event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
-			} else {
-				$GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->getId(), $serviceClass->getCustid());
-				$serviceClass->set_ip($event['newip'])->save();
-				$event['status'] = 'ok';
-				$event['status_text'] = 'The IP Address has been changed.';
-			}
-			$event->stopPropagation();
-		}
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getChangeIp(GenericEvent $event)
+    {
+        if ($event['category'] == get_service_define('PARALLELS')) {
+            $serviceClass = $event->getSubject();
+            $settings = get_module_settings(self::$module);
+            $parallels = new \Parallels(FANTASTICO_USERNAME, FANTASTICO_PASSWORD);
+            myadmin_log(self::$module, 'info', 'IP Change - (OLD:'.$serviceClass->getIp().") (NEW:{$event['newip']})", __LINE__, __FILE__, self::$module, $serviceClass->getId());
+            $result = $parallels->editIp($serviceClass->getIp(), $event['newip']);
+            if (isset($result['faultcode'])) {
+                myadmin_log(self::$module, 'error', 'Parallels editIp('.$serviceClass->getIp().', '.$event['newip'].') returned Fault '.$result['faultcode'].': '.$result['fault'], __LINE__, __FILE__, self::$module, $serviceClass->getId());
+                $event['status'] = 'error';
+                $event['status_text'] = 'Error Code '.$result['faultcode'].': '.$result['fault'];
+            } else {
+                $GLOBALS['tf']->history->add($settings['TABLE'], 'change_ip', $event['newip'], $serviceClass->getId(), $serviceClass->getCustid());
+                $serviceClass->set_ip($event['newip'])->save();
+                $event['status'] = 'ok';
+                $event['status_text'] = 'The IP Address has been changed.';
+            }
+            $event->stopPropagation();
+        }
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getRequirements(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Plugins\Loader $this->loader
-		 */
-		$loader = $event->getSubject();
-		$loader->add_requirement('class.Parallels', '/../vendor/detain/parallels-licensing/src/Parallels.php', '\\Detain\\Parallels\\');
-		$loader->add_requirement('activate_parallels', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
-		$loader->add_requirement('deactivate_parallels', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
-		$loader->add_requirement('deactivate_parallels_by_key', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getRequirements(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Plugins\Loader $this->loader
+         */
+        $loader = $event->getSubject();
+        $loader->add_requirement('class.Parallels', '/../vendor/detain/parallels-licensing/src/Parallels.php', '\\Detain\\Parallels\\');
+        $loader->add_requirement('activate_parallels', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
+        $loader->add_requirement('deactivate_parallels', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
+        $loader->add_requirement('deactivate_parallels_by_key', '/../vendor/detain/myadmin-parallels-licensing/src/parallels.inc.php');
+    }
 
-	/**
-	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
-	 */
-	public static function getSettings(GenericEvent $event)
-	{
-		/**
-		 * @var \MyAdmin\Settings $settings
-		 **/
-		$settings = $event->getSubject();
-		$settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_client', _('Parallels KA Client'), _('Parallels KA Client'), $settings->get_setting('PARALLELS_KA_CLIENT'));
-		$settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_login', _('Parallels KA Login'), _('Parallels KA Login'), $settings->get_setting('PARALLELS_KA_LOGIN'));
-		$settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_password', _('Parallels KA Password'), _('Parallels KA Password'), $settings->get_setting('PARALLELS_KA_PASSWORD'));
-		$settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_url', _('Parallels KA URL'), _('Parallels KA URL'), $settings->get_setting('PARALLELS_KA_URL'));
-		$settings->add_dropdown_setting(self::$module, _('Parallels'), 'outofstock_licenses_parallels', _('Out Of Stock Parallels Licenses'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_LICENSES_FANTASTICO'), ['0', '1'], ['No', 'Yes']);
-	}
+    /**
+     * @param \Symfony\Component\EventDispatcher\GenericEvent $event
+     */
+    public static function getSettings(GenericEvent $event)
+    {
+        /**
+         * @var \MyAdmin\Settings $settings
+         **/
+        $settings = $event->getSubject();
+        $settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_client', _('Parallels KA Client'), _('Parallels KA Client'), $settings->get_setting('PARALLELS_KA_CLIENT'));
+        $settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_login', _('Parallels KA Login'), _('Parallels KA Login'), $settings->get_setting('PARALLELS_KA_LOGIN'));
+        $settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_password', _('Parallels KA Password'), _('Parallels KA Password'), $settings->get_setting('PARALLELS_KA_PASSWORD'));
+        $settings->add_text_setting(self::$module, _('Parallels'), 'parallels_ka_url', _('Parallels KA URL'), _('Parallels KA URL'), $settings->get_setting('PARALLELS_KA_URL'));
+        $settings->add_dropdown_setting(self::$module, _('Parallels'), 'outofstock_licenses_parallels', _('Out Of Stock Parallels Licenses'), _('Enable/Disable Sales Of This Type'), $settings->get_setting('OUTOFSTOCK_LICENSES_FANTASTICO'), ['0', '1'], ['No', 'Yes']);
+    }
 }
--- src/parallels.inc.php
+++ src/parallels.inc.php
@@ -23,56 +23,56 @@
  */
 function activate_parallels($ipAddress, $type, $addons = '')
 {
-	function_requirements('class.Parallels');
-	myadmin_log('licenses', 'info', "Parallels New License {$ipAddress} Type {$type} Addons {$addons} called", __LINE__, __FILE__);
-	ini_set('max_execution_time', 1000); // just put a lot of time
-	ini_set('default_socket_timeout', 1000); // same
-	$parallels = new \Detain\Parallels\Parallels();
-	if (trim($addons) == '') {
-		$addonsArray = [];
-	} else {
-		$addonsArray = explode(',', $addons);
-	}
+    function_requirements('class.Parallels');
+    myadmin_log('licenses', 'info', "Parallels New License {$ipAddress} Type {$type} Addons {$addons} called", __LINE__, __FILE__);
+    ini_set('max_execution_time', 1000); // just put a lot of time
+    ini_set('default_socket_timeout', 1000); // same
+    $parallels = new \Detain\Parallels\Parallels();
+    if (trim($addons) == '') {
+        $addonsArray = [];
+    } else {
+        $addonsArray = explode(',', $addons);
+    }
 
-	// check if already active
-	myadmin_log('licenses', 'info', 'addonsArray:', __LINE__, __FILE__);
-	myadmin_log('licenses', 'info', var_export($addonsArray, true), __LINE__, __FILE__);
-	$request = [$type, $addonsArray, $ipAddress];
-	try {
-		$response = $parallels->createKey($type, $addonsArray, $ipAddress);
-		request_log('licenses', false, __FUNCTION__, 'parallels', 'createKey', $request, $response);
-		myadmin_log('licenses', 'info', "activate Parallels({$ipAddress}, {$type}, {$addons}) Response: ".json_encode($response), __LINE__, __FILE__);
-	} catch (\XML_RPC2_CurlException  $e) {
-		request_log('licenses', false, __FUNCTION__, 'parallels', 'createKey', $request, $e->getMessage());
-		return false;
-	}
-	/* example response:
-	Array(
-	[mainKeyNumber] => PLSK.00007677.0000
-	[expirationDate] => stdClass Object
-	(
-	[scalar] => 20131211T00:00:00
-	[xmlrpc_type] => datetime
-	[timestamp] => 1386720000
-	)
+    // check if already active
+    myadmin_log('licenses', 'info', 'addonsArray:', __LINE__, __FILE__);
+    myadmin_log('licenses', 'info', var_export($addonsArray, true), __LINE__, __FILE__);
+    $request = [$type, $addonsArray, $ipAddress];
+    try {
+        $response = $parallels->createKey($type, $addonsArray, $ipAddress);
+        request_log('licenses', false, __FUNCTION__, 'parallels', 'createKey', $request, $response);
+        myadmin_log('licenses', 'info', "activate Parallels({$ipAddress}, {$type}, {$addons}) Response: ".json_encode($response), __LINE__, __FILE__);
+    } catch (\XML_RPC2_CurlException  $e) {
+        request_log('licenses', false, __FUNCTION__, 'parallels', 'createKey', $request, $e->getMessage());
+        return false;
+    }
+    /* example response:
+    Array(
+    [mainKeyNumber] => PLSK.00007677.0000
+    [expirationDate] => stdClass Object
+    (
+    [scalar] => 20131211T00:00:00
+    [xmlrpc_type] => datetime
+    [timestamp] => 1386720000
+    )
 
-	[productKey] => A00300-K4KT02-JHE757-B1FE76-JD2N77
-	[additionalKeysNumbers] => Array
-	(
-	)
+    [productKey] => A00300-K4KT02-JHE757-B1FE76-JD2N77
+    [additionalKeysNumbers] => Array
+    (
+    )
 
-	[resultCode] => 100
-	[resultDesc] => PLSK.00007677.0000 has been successfully created.
-	[updateDate] => stdClass Object
-	(
-	[scalar] => 20131201T00:00:00
-	[xmlrpc_type] => datetime
-	[timestamp] => 1385856000
-	)
+    [resultCode] => 100
+    [resultDesc] => PLSK.00007677.0000 has been successfully created.
+    [updateDate] => stdClass Object
+    (
+    [scalar] => 20131201T00:00:00
+    [xmlrpc_type] => datetime
+    [timestamp] => 1385856000
+    )
 
-	)
-	*/
-	return $response;
+    )
+    */
+    return $response;
 }
 
 
@@ -81,26 +81,26 @@ function activate_parallels($ipAddress, $type, $addons = '')
 */
 function deactivate_parallels($ipAddress)
 {
-	myadmin_log('licenses', 'info', "Parallels Deactivation ({$ipAddress})", __LINE__, __FILE__);
-	function_requirements('class.Parallels');
-	$parallels = new \Detain\Parallels\Parallels();
-	try {
-		$response = $parallels->getKeyNumbers($ipAddress);
-		request_log('licenses', false, __FUNCTION__, 'parallels', 'getMainKeyFromIp', $ipAddress, $response);
-		myadmin_log('licenses', 'info', "Parallels getMainKeyFromIp({$ipAddress}): ".json_encode($response), __LINE__, __FILE__);
-	} catch (\XML_RPC2_CurlException  $e) {
-		request_log('licenses', false, __FUNCTION__, 'parallels', 'getMainKeyFromIp', $ipAddress, $e->getMessage());
-		return false;
-	}
-	if (isset($response['keyNumbers'])) {
-		$keys = $response['keyNumbers'];
-		foreach ($keys as $key) {
-			deactivate_parallels_by_key($key);
-		}
-	} else {
-		myadmin_log('licenses', 'info', 'Parallels No Key Found to Terminate', __LINE__, __FILE__);
-	}
-	return true;
+    myadmin_log('licenses', 'info', "Parallels Deactivation ({$ipAddress})", __LINE__, __FILE__);
+    function_requirements('class.Parallels');
+    $parallels = new \Detain\Parallels\Parallels();
+    try {
+        $response = $parallels->getKeyNumbers($ipAddress);
+        request_log('licenses', false, __FUNCTION__, 'parallels', 'getMainKeyFromIp', $ipAddress, $response);
+        myadmin_log('licenses', 'info', "Parallels getMainKeyFromIp({$ipAddress}): ".json_encode($response), __LINE__, __FILE__);
+    } catch (\XML_RPC2_CurlException  $e) {
+        request_log('licenses', false, __FUNCTION__, 'parallels', 'getMainKeyFromIp', $ipAddress, $e->getMessage());
+        return false;
+    }
+    if (isset($response['keyNumbers'])) {
+        $keys = $response['keyNumbers'];
+        foreach ($keys as $key) {
+            deactivate_parallels_by_key($key);
+        }
+    } else {
+        myadmin_log('licenses', 'info', 'Parallels No Key Found to Terminate', __LINE__, __FILE__);
+    }
+    return true;
 }
 
 /**
@@ -108,34 +108,35 @@ function deactivate_parallels($ipAddress)
 */
 function deactivate_parallels_by_key($key)
 {
-	myadmin_log('licenses', 'info', "Parallels Deactivation ({$key})", __LINE__, __FILE__);
-	function_requirements('class.Parallels');
-	$parallels = new \Detain\Parallels\Parallels();
-	$status = json_decode(file_get_contents(__DIR__.'/../../../../include/config/plesk.json'), true);
-	if (count(explode('.', $key)) > 2)
-		$key = substr($key, 0, strrpos($key, '.'));
-	$response = $parallels->terminateKey($key);
+    myadmin_log('licenses', 'info', "Parallels Deactivation ({$key})", __LINE__, __FILE__);
+    function_requirements('class.Parallels');
+    $parallels = new \Detain\Parallels\Parallels();
+    $status = json_decode(file_get_contents(__DIR__.'/../../../../include/config/plesk.json'), true);
+    if (count(explode('.', $key)) > 2) {
+        $key = substr($key, 0, strrpos($key, '.'));
+    }
+    $response = $parallels->terminateKey($key);
 
-	request_log('licenses', false, __FUNCTION__, 'parallels', 'terminateKey', $key, $response);
-	myadmin_log('licenses', 'info', "Parallels TerminateKey({$key}) Response: ".json_encode($response), __LINE__, __FILE__);
-	if (array_key_exists($key, $status)) {
-		$status[$key]['terminated'] = true;
-		file_put_contents(__DIR__.'/../../../../include/config/plesk.json', json_encode($status, JSON_PRETTY_PRINT));
-	}
-	if (array_key_exists(str_replace('0001', '0000', $key), $status)) {
-		$status[str_replace('0001', '0000', $key)]['terminated'] = true;
-		file_put_contents(__DIR__.'/../../../../include/config/plesk.json', json_encode($status, JSON_PRETTY_PRINT));
-	}
-	if (!isset($status[$key]['terminated'])  || !$status[$key]['terminated']) {
-		$bodyRows = [];
-		$bodyRows[] = 'License Key: '.$key.' unable to deactivate.';
-		$bodyRows[] = 'Deactivation Response: .'.json_encode($response);
-		$subject = 'Parallels License Deactivation Issue Key: '.$key;
-		$smartyE = new TFSmarty;
-		$smartyE->assign('h1', 'Parallels License Deactivation');
-		$smartyE->assign('body_rows', $bodyRows);
-		$msg = $smartyE->fetch('email/client/client_email.tpl');
-		(new \MyAdmin\Mail())->adminMail($subject, $msg, false, 'client/client_email.tpl');
-	}
-	return true;
+    request_log('licenses', false, __FUNCTION__, 'parallels', 'terminateKey', $key, $response);
+    myadmin_log('licenses', 'info', "Parallels TerminateKey({$key}) Response: ".json_encode($response), __LINE__, __FILE__);
+    if (array_key_exists($key, $status)) {
+        $status[$key]['terminated'] = true;
+        file_put_contents(__DIR__.'/../../../../include/config/plesk.json', json_encode($status, JSON_PRETTY_PRINT));
+    }
+    if (array_key_exists(str_replace('0001', '0000', $key), $status)) {
+        $status[str_replace('0001', '0000', $key)]['terminated'] = true;
+        file_put_contents(__DIR__.'/../../../../include/config/plesk.json', json_encode($status, JSON_PRETTY_PRINT));
+    }
+    if (!isset($status[$key]['terminated'])  || !$status[$key]['terminated']) {
+        $bodyRows = [];
+        $bodyRows[] = 'License Key: '.$key.' unable to deactivate.';
+        $bodyRows[] = 'Deactivation Response: .'.json_encode($response);
+        $subject = 'Parallels License Deactivation Issue Key: '.$key;
+        $smartyE = new TFSmarty();
+        $smartyE->assign('h1', 'Parallels License Deactivation');
+        $smartyE->assign('body_rows', $bodyRows);
+        $msg = $smartyE->fetch('email/client/client_email.tpl');
+        (new \MyAdmin\Mail())->adminMail($subject, $msg, false, 'client/client_email.tpl');
+    }
+    return true;
 }
